13ba233250dcd7f6f3535a7cac836b4032c4a5dd
Leonard Siebeneicher <leonard7e@googlemail.com>
2016-06-25 21:44:49 +0200

    Code refracturing

--- watchgit.py
+++ watchgit.py
@@ -1,26 +1,44 @@
 #!/usr/bin/python3
 # WatchGit version 0.0.1
 
-import pyinotify
-import argparse
 import time
+import pyinotify
 import git
+import argparse
+
+
+# 
+# VCS Systems we can use to backup data
+# 
+
+class EchoCVS:
+    def __init__(self, repository):
+        self.repository = git.Repo(repository)
+    def fCreated(self, path):
+        print("Added:", path)
+    def fModified(self, path):
+        print("Modified:", path)
+    def fRemoved(self, path):
+        print("Removed:", path)
+    def fRenamed(self, src, dest):
+        print("Renamed:", src, dest)
+    def recordChanges(self):
+        print("Record Changes:", time.localtime())
+    def remoteMigrate(self):
+        print("Migrating")
+
 
 class GitVersioning:
     def __init__(self, repository):
         self.remoteRepository=remoteUrl
-        self.repository = git.Repo.init(repository)
+        self.repository = git.Repo(repository)
     def fCreated(self, path):
-        print("git add", path)
         self.repository.index.add(path)
     def fModified(self, path):
-        print("git add", path)
         self.repository.index.add(path)
     def fRemoved(self, path):
-        print("git rm", path)
         self.repository.index.remove(path)
     def fRenamed(self, src, dest):
-        print("git mv", src, dest)
         self.repository.index.move([src,dest])
     def recordChanges(self):
         print("git commit -m", time.localtime())
@@ -29,27 +47,44 @@ class GitVersioning:
         print("git push")
         # self.repository.index.commit( .... )
         
-class VersionHandler(pyinotify.ProcessEvent):
-    def __init__(self, versioning):
-        self.versioning = versioning
+def chose_version_handler(args):
+    v = args.vcs
+    if v == 'git':
+        print("Using GIT")
+        return GitVersioning(args.repository)
+    elif v == 'echo':
+        print("Using ECHO")
+        return EchoCVS(args.repository)
+    else:
+        sys.exit("Versioning not supportet: " + v)
 
+
+# 
+# We derive VcsHandler from pyinotify.ProcessEvend.
+# 
+
+class VcsHandler(pyinotify.ProcessEvent):
+    def __init__(self, vcs):
+        self.vcs = vcs
+        
     def process_IN_CREATE(self, event):
-        self.versioning.fCreated(event.pathname)
+        self.vcs.fCreated(event.pathname)
 
     def process_IN_DELETE(self, event):
-        self.versioning.fRemoved(event.pathname)
+        self.vcs.fRemoved(event.pathname)
 
     def process_IN_MODIFY(self, event):
-        self.versioning.fModified(event.pathname)
+        self.vcs.fModified(event.pathname)
 
     def process_IN_MOVED_FROM(self, event):
         self.move_from = event.pathname
 
     def process_IN_MOVED_TO(self, event):
-        self.versioning.fRenamed(self.move_from, event.pathname)
+        self.vcs.fRenamed(self.move_from, event.pathname)
 
-
-# ---
+#
+# Argument Parsing comes here
+#
 
 def parse_app_arguments():
     def create_parser():
@@ -58,28 +93,55 @@ def parse_app_arguments():
         return argparse.ArgumentParser(description=descr) 
     def register_args(parser):
         parser.add_argument('repository', metavar='<local>')
-        parser.add_argument('remote', metavar='<remote>')
-        parser.add_argument('-V', '--versioning', dest='versioning', help='Chosie versioning system. Default will be GIT.')
-    # --- parse_app_arguments ---
-        
+        # parser.add_argument('remote', metavar='<remote>')
+        parser.add_argument('-V', '--vcs', dest='vcs', default='git',
+                help='Chosie vcs system. Default will be GIT.')
+
+    # After defining some subroutines, lets perform argument parsing        
     parser = create_parser()
     register_args(parser)
     return parser.parse_args()
 
-# ---
+# 
+# The Main routine
+# 
+
 def main():
+    def init_pyinotify(args, watchmanager):        
+        mask = pyinotify.IN_DELETE | pyinotify.IN_CREATE | pyinotify.IN_MODIFY | pyinotify.IN_MOVED_FROM | pyinotify.IN_MOVED_TO
+        watch = watchmanager.add_watch(args.repository, mask, rec=True )
+
+        notifier = pyinotify.ThreadedNotifier(watchmanager, handler)
+        notifier.start()
+        
+        # watchmanager.rm_watch(watch.values())
+        return watch, notifier
+
+    def app_loop(args, V, notifier):
+        while(True):
+            time.sleep(60*1) # 60*15
+            # test whether we need to commit changes
+            if (True): 
+                V.recordChanges()
+                V.remoteMigrate()
+                
+                notifier.stop()
+
+    # TODO: Versionierung aus ArgumentParser beziehen.
     args = parse_app_arguments()
+    V = chose_version_handler(args)
+    handler = VcsHandler( V )
+    watchmanager = pyinotify.WatchManager()
+    
+    watch, notifier = init_pyinotify(args, watchmanager)
+    app_loop(args, V, notifier)
 
-    versioning = GitVersioning(args.repository, args.remote)
-    handler = VersionHandler(versioning)
 
-    wm = pyinotify.WatchManager()
-    notifier = pyinotify.Notifier(wm, handler)
 
-    mask = pyinotify.IN_DELETE | pyinotify.IN_CREATE | pyinotify.IN_MODIFY | pyinotify.IN_MOVED_FROM | pyinotify.IN_MOVED_TO
-    wdd = wm.add_watch(args.repository, mask, rec=True)
-
-    notifier.loop()
 
 if __name__ == '__main__':
     main()
+
+
+# TODO
+# - test whether we need to commit changes
